f864d1de51ccc7817041a0f39607384d654e943a
Renaud Pacalet <renaud.pacalet@telecom-paristech.fr>
2024-02-22 09:17:10 +0100

    Checkpoint lab01

---
+++ vhdl/lab01/ct.params.tcl
@@ -0,0 +1,20 @@
+#
+# Copyright © Telecom Paris
+# Copyright © Renaud Pacalet (renaud.pacalet@telecom-paris.fr)
+# 
+# This file must be used under the terms of the CeCILL. This source
+# file is licensed as described in the file COPYING, which you should
+# have received as part of this distribution. The terms are also
+# available at:
+# https://cecill.info/licences/Licence_CeCILL_V2.1-en.html
+#
+
+array set ios {
+	switch0       {}
+	wire_in       {}
+	wire_out      {}
+	led[0]        {}
+	led[1]        {}
+	led[2]        {}
+	led[3]        {}
+}
