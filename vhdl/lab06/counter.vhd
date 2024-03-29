ee18d7bcd73f371680d5a1deb0c9d3c7b6740147
Brice <robert@eurecom.fr>
2024-03-29 11:04:14 +0100

    .

---
+++ vhdl/lab06/counter.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; -- For using unsigned arithmetic
+
+entity counter is
+    generic(
+        f_mhz:  positive range 1 to 1000 := 100; -- Clock frequency in MHz
+        max_us: natural := 10 -- Maximum duration in microseconds
+    );
+    port(
+        clk:      in  std_ulogic;  -- Clock signal
+        sresetn:  in  std_ulogic;  -- Synchronous reset, active low
+        tz:       in  std_ulogic;  -- Timer control signal (assumed as start control)
+        t:        out natural range 0 to max_us -- Timer value in microseconds
+    );
+end entity timer;
+
+architecture rtl of counter is
+
+end architecture rtl;
